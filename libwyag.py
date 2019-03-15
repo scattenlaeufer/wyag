1c22088bd1b806793e11c2b6c0f9f55e10051001
Björn Guth <bjoern.guth@rwth-aachen.de>
2019-03-15 14:19:38 +0100

    Create function to find git directory

--- libwyag.py
+++ libwyag.py
@@ -130,6 +130,23 @@ def repo_default_config():
     return ret
 
 
+def repo_find(path=".", required=True):
+    path=os.path.realpath(path)
+
+    if os.path.isdir(os.path.join(path, ".git")):
+        return GitRepository(path)
+
+    parent = os.path.realpath(os.path.join(path, ".."))
+
+    if parent == path:
+        if required:
+            raise Exception("No a git director.")
+        else:
+            return None
+
+    return repo_find(parent, required)
+
+
 def repo_path(repo, *path):
     """compute path under repo's gitdir."""
     return os.path.join(repo.gitdir, *path)
