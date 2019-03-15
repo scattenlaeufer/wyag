9d04aede0a202d85746358eb2d83c07fa88d2e14
Björn Guth <bjoern.guth@rwth-aachen.de>
2019-03-15 04:27:42 +0100

    Create first step of a Git repository abstraction

--- libwyag.py
+++ libwyag.py
@@ -53,6 +53,35 @@ def main(argv=sys.argv[1:]):
         cmd_tag(args)
 
 
+class GitRepository(object):
+    """A git repository"""
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a Git repository {}".format(path))
+
+        # Read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0 and not force:
+                raise Exception("Unsupported repositoryformatversion {}".format(vers))
+
+
 def cmd_add(args):
     pass
 
