620b2950579ad4bf71dca6113d28cd0823c8ebdb
Björn Guth <bjoern.guth@rwth-aachen.de>
2019-03-15 04:30:23 +0100

    Add some uitlity functions to compute paths

--- libwyag.py
+++ libwyag.py
@@ -82,6 +82,36 @@ class GitRepository(object):
                 raise Exception("Unsupported repositoryformatversion {}".format(vers))
 
 
+def repo_path(repo, *path):
+    """compute path under repo's gitdir."""
+    return os.path.join(repo.gitdir, *path)
+
+
+def repo_file(repo, *path, mkdir=False):
+    """Same as reo_path, but create direname(*path) if absent. For example repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create .git/refs/remotes/origin."""
+
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+
+def repo_dir(repo, *path, mkdir=False):
+    """Same as repo_path, but mkdir *path if absent if mkdir."""
+
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            raise Exception("Not a directory {}".format(path))
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+
 def cmd_add(args):
     pass
 
